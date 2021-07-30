bf768b282bb51e8de7e56327bf7df7ae0f44671a
Gustavo Souza <gustavo.de.souza@outlook.com>
2021-07-30 17:10:51 -0300

    mobile v1

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,37 +1,37 @@
 export const projects = [
 {
-    "title":"Titulo 1",
-    "description":"descrição 1",
-    "image":"./projects/images/e-wallet.png",
-    "link": "link 1"
+    "title":"Apresentação Carteira Digital",
+    "description":"Criação de interface para fintech de beneficios corporativos.",
+    "image":"./projects/images/E-wallet.png",
+        "link": "https://www.behance.net/gallery/124258047/E-wallet-Carteira-Digital"
 },
 {
-    "title":"Titulo 1",
-    "description":"descrição 1",
+    "title":"Sign Up",
+    "description":"Criação de tela de Sign Up, com opção de Criar Conta e Fazer Log in.",
     "image":"./projects/images/DailyUI-001-Presentation.png",
-    "link": "link 1"
+    "link": "https://www.behance.net/gallery/123683913/Daily-UI-002-Credit-Card-Checkout"
 },
 {
-    "title":"Titulo 2",
-    "description":"descrição 1",
+    "title":"Credit Card Checkout",
+    "description":"Checkout de cartão de crédito seguindo o princípio da continuidade e focando na experiência do usuário.",
     "image":"./projects/images/DailyUI-002-Presentation.png",
-    "link": "link 1"
+    "link": "https://www.behance.net/gallery/123683913/Daily-UI-002-Credit-Card-Checkout"
 },
 {
-    "title":"Titulo 3",
-    "description":"descrição 1",
+    "title":"Landing Page",
+    "description":"Landing Page de curso de Francês que apresenta sua metodologia e benefícios.",
     "image":"./projects/images/DailyUI-003-Presentation.png",
-    "link": "link 1"
+    "link": "https://www.behance.net/gallery/123798117/Daily-UI-003-Landing-Page"
 },
 {
-    "title":"Titulo 4",
-    "description":"descrição 1",
+    "title":"Calculator",
+    "description":"Releitura de calculadora mobile com foco em minimalismo e objetividade.",
     "image":"./projects/images/DailyUI-004-Presentation.png",
-    "link": "link 1"
+    "link": "https://www.behance.net/gallery/123861253/Daily-UI-004-Calculator"
 },
 {
-    "title":"Titulo 1",
-    "description":"descrição 1",
+    "title":"App Icon",
+    "description":"Ícone de aplicativo para ajudar a dormir com inspirações em ícones atuais.",
     "image":"./projects/images/DailyUI-005-Presentation.png",
     "link": "link 1"
 }]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
 import { projects } from './data.js'
 
-const ChangeBook = (_ => {
+const Render = (_ => {
 
 // cache the DOM //
 const $portfolio = document.querySelector(".portfolio");
-const $project = document.querySelector(".portfolio__project");
+const $hamburger = document.querySelector(".header__mobile-hamburger");
+const $mobileMenu = document.querySelector(".header__mobile-menu");
+
+// toggle mobile menu
+
+$hamburger.addEventListener("click", _ => {
+            $mobileMenu.classList.toggle('header__mobile-menu-open');
+        })
+
 
 // create project
     let markup = "";
@@ -16,7 +24,7 @@ const $project = document.querySelector(".portfolio__project");
             <div class="portfolio__text-container">
             <h2>${projectObj.title}</h2>
             <p>${projectObj.description}</p>
-            <a href="${projectObj.link}"><img src="./images/behance-icon.png" alt="behance-icon"></a>
+            <a href="${projectObj.link}" target="_blank">Ver projeto</a>
             </div>
         </div>
         `
