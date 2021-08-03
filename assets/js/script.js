0d91be7d56379d17493013b51a0f8f08151cc8aa
Gustavo Souza <gustavo.de.souza@outlook.com>
2021-08-03 16:23:02 -0300

    desktop mobile finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,36 @@ const Render = (_ => {
 const $portfolio = document.querySelector(".portfolio");
 const $hamburger = document.querySelector(".header__mobile-hamburger");
 const $mobileMenu = document.querySelector(".header__mobile-menu");
+const $aboutBttn = document.querySelector(".header__menu-sobre-mim");
+const $about = document.querySelector(".about");
+const $projects = document.querySelector(".header__menu-projetos");
+const $footer = document.querySelector(".footer");
 
 // toggle mobile menu
 
 $hamburger.addEventListener("click", _ => {
             $mobileMenu.classList.toggle('header__mobile-menu-open');
         })
+
+// about
+
+$aboutBttn.addEventListener("click", _ => {
+        $portfolio.classList.remove("portfolio-hide");
+        $portfolio.classList.add("portfolio-hide");
+        $about.style.display = 'flex';
+        $aboutBttn.style.fontWeight = '600';
+        $projects.style.fontWeight = '400';
+        $footer.style.position = 'absolute';
+});
+
+$projects.addEventListener("click", _ => {
+        $portfolio.classList.add("portfolio-hide");
+        $portfolio.classList.remove("portfolio-hide");
+        $about.style.display = 'none';
+        $aboutBttn.style.fontWeight = '400';
+        $projects.style.fontWeight = '600';
+        $footer.style.position = 'relative';
+});
 
 
 // create project
